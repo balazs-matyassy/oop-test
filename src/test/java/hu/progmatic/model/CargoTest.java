dae825848fb2071c34e8314e0976260adcf42745
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-05-26 17:49:01 +0200

    bm: update

--- src/test/java/hu/progmatic/model/CargoTest.java
+++ src/test/java/hu/progmatic/model/CargoTest.java
@@ -6,9 +6,35 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class CargoTest {
     @Test
-    public void whenCargoCreated_thenDefaultConstructorOk() {
+    public void whenDefaultConstructorUsed_thenValuesOk() {
         Cargo cargo = new Cargo();
 
         assertNull(cargo.getDate());
+        assertNull(cargo.getSource());
+        assertNull(cargo.getDestination());
+        assertNull(cargo.getType());
+        assertEquals(0, cargo.getValue());
+    }
+
+    @Test
+    public void whenValuesConstructorUsed_thenValuesOk() {
+        Cargo cargo = new Cargo("2250.01.01", "Hold", "Ganymedes", "Nyersanyagok", 353);
+
+        assertEquals("2250.01.01", cargo.getDate());
+        assertEquals("Hold", cargo.getSource());
+        assertEquals("Ganymedes", cargo.getDestination());
+        assertEquals("Nyersanyagok", cargo.getType());
+        assertEquals(353, cargo.getValue());
+    }
+
+    @Test
+    public void whenLineConstructorUsed_thenValuesOk() {
+        Cargo cargo = new Cargo("2250.01.01;Hold;Ganymedes;Nyersanyagok;353");
+
+        assertEquals("2250.01.01", cargo.getDate());
+        assertEquals("Hold", cargo.getSource());
+        assertEquals("Ganymedes", cargo.getDestination());
+        assertEquals("Nyersanyagok", cargo.getType());
+        assertEquals(353, cargo.getValue());
     }
 }
