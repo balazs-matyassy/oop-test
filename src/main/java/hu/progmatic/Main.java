9bc270816a0dcb0f2ec0a95db8b82ca99c0b25e0
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-05-26 17:26:28 +0200

    bm: update

---
+++ src/main/java/hu/progmatic/Main.java
@@ -0,0 +1,12 @@
+package hu.progmatic;
+
+public class Main {
+    public static void main(String[] args) {
+        // 1. Hozzunk létre egy Cargo osztályt
+        // 2. Legyen 3 konstruktor
+        //      - default (TEST)
+        //      - minden értéket beállító (TEST)
+        //      - a csv fájl egy sorából minden értéket beállító (TEST)
+        // 3. Legyenek getterek + setterek
+    }
+}
---
+++ src/main/java/hu/progmatic/model/Cargo.java
@@ -0,0 +1,69 @@
+package hu.progmatic.model;
+
+public class Cargo {
+    private String date;
+    private String source;
+    private String destination;
+    private String type;
+    private int value;
+
+    public Cargo() {
+    }
+
+    public Cargo(String date, String source, String destination, String type, int value) {
+        this.date = date;
+        this.source = source;
+        this.destination = destination;
+        this.type = type;
+        this.value = value;
+    }
+
+    public Cargo(String csvLine) {
+        String[] values = csvLine.split(";");
+        this.date = values[0];
+        this.source = values[1];
+        this.destination = values[2];
+        this.type = values[3];
+        this.value = Integer.parseInt(values[4]);
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    public void setValue(int value) {
+        this.value = value;
+    }
+}
