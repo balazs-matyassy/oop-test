b4c2c7d13ebeb5a55f5a64f95450d09af078b295
Balázs Mátyássy <balazs.matyassy@hotmail.com>
2022-05-30 18:12:19 +0200

    bm: update

---
+++ src/main/java/hu/progmatic/constructor/Person.java
@@ -0,0 +1,38 @@
+package hu.progmatic.constructor;
+
+public class Person {
+    private String name;
+    private int birthYear;
+    private int age;
+
+    // 1. nincs visszatérési érték
+    // 2. a metódus megegyezik az osztály nevével
+    // 3. nem kötelező public láthatóságot kapnia (lehet pl. protected, package, private)
+    //      ha pl. package a láthatóság, akkor csomagon kívül nem működik a new Person() hívás
+    // new Person();
+    public Person() {
+
+    }
+
+    // overloading (túlterhelés)
+    // a ténylegesen meghívott konstruktort a szignatúra határozza meg
+    // new Person("Pista");
+    public Person(String name) {
+        this.name = name;
+    }
+
+    // new Person("Pista", 1990);
+    public Person(String name, int birthYear) {
+        this.name = name;
+        this.birthYear = birthYear;
+        // nem kötelező mindig mindegyik nem final mezőt beállítani
+        this.age = 2022 - birthYear;
+    }
+
+    // new Person("Pista", 1990, 32);
+    public Person(String name, int birthYear, int age) {
+        this.name = name;
+        this.birthYear = birthYear;
+        this.age = age;
+    }
+}
